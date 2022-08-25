1365a684c93401fd91e4754f263aaa9a95a3c1c7
DA-11 <56694553+DA-11@users.noreply.github.com>
2022-08-25 16:48:27 +0530

    Update Account.java

--- 00_bankapp/src/main/java/com/bankapp/model/entity/Account.java
+++ 00_bankapp/src/main/java/com/bankapp/model/entity/Account.java
@@ -16,7 +16,7 @@ public class Account {
 
 	public void setEmail(String email) {
 		
-		System.out.println("HELLO FROM GIT1");
+		System.out.println("HELLO FROM GIT111");
 		this.email = email;
 	}
 
